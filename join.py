198e0b93e4490b1f7f3d8dcc80f994db6d27a110
Tanmay choudhari <89440891+tannmayyy@users.noreply.github.com>
2025-01-30 20:15:36 +0530

    Update join.py

--- join.py
+++ join.py
@@ -1,12 +1,2 @@
-CREATE OR REPLACE ROW ACCESS POLICY trades_row_policy 
-AS (entity STRING)
-RETURNS BOOLEAN ->
-EXISTS (
-    SELECT 1 
-    FROM user_access_mapping 
-    WHERE user_name = CURRENT_USER()
-      AND (
-          category_access = 'NSL'  -- If user has NSL access, allow all
-          OR category_access = entity -- Otherwise, only show their specific entity
-      )
-);
+ALTER TABLE trades 
+ADD ROW ACCESS POLICY trades_row_policy ON (entity);
